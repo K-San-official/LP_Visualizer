8b7b35005b1f8a5c6b33cbbb6b88270ed8ddb0d2
K-San (Laptop) <konstantin.sandfort@gmail.com>
2021-12-20 14:15:28 +0100

    added hello world demo window for JavaFX-test

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,6 +1,11 @@
 package app;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +16,6 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
 
     /**
      * The main entry point for all JavaFX applications.
@@ -29,7 +33,21 @@ public class App extends Application {
      * @throws Exception if something goes wrong
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        System.out.println("It works!");
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
