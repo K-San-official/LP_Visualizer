1f7bc8c3e72aa85d264cf558bfc2336363acce4c
S010MON <leon@bdebnath.com>
2021-12-28 17:39:49 +0000

    adds pivot class

---
+++ src/main/java/model/Pivot.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Pivot
+{
+    public int row;
+    public int col;
+
+    public Pivot(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+}
