32cb78656c2cf71f64aaaff1079300efe1a359ed
K-San (Laptop) <konstantin.sandfort@gmail.com>
2021-12-29 19:01:53 +0100

    augmented input window

--- src/main/java/gui/InputWindow.java
+++ src/main/java/gui/InputWindow.java
@@ -1,8 +1,13 @@
 package gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 /**
  * This class represents a window where the user can input the
@@ -11,14 +16,29 @@ import javafx.stage.Stage;
 public class InputWindow extends Stage {
 
     // Variables
-    int windowWidth = 300;
-    int windowHeight = 300;
+    int windowWidth = 400;
+    int windowHeight = 400;
 
     /**
      * Constructor
      */
     public InputWindow() {
         this.setTitle("Add new linear program");
-        this.setScene(new Scene(new BorderPane(), windowWidth, windowHeight));
+        GridPane grid = lpGridLayout();
+
+        this.setScene(new Scene(grid, windowWidth, windowHeight));
+        this.show();
+    }
+
+    private GridPane lpGridLayout() {
+        GridPane gridPane = new GridPane();
+        TextField textField = new TextField("Input");
+
+        Text text = new Text("Text");
+        gridPane.add(textField, 0, 0);
+        gridPane.add(text, 1, 0);
+
+
+        return gridPane;
     }
 }
